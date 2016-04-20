ee184d57f28e9eecd55423657d496d16e6a80407
carlosiiii <carlos_nompodo@hotmail.com>
2016-04-20 17:11:20 +0200

    commit test class

---
+++ app/src/androidTest/java/com/cigsa/carlos/calcar4/Carlcar4MainActivityTest.java
@@ -0,0 +1,19 @@
+package com.cigsa.carlos.calcar4;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Carlos on 20/04/2016.
+ */
+public class Carlcar4MainActivityTest {
+
+    @org.junit.Test
+    public void testDameCadenaConexion() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testIsWifiConnected() throws Exception {
+
+    }
+}
---
+++ app/src/androidTest/java/com/cigsa/carlos/calcar4/ExampleTest.java
@@ -0,0 +1,7 @@
+package com.cigsa.carlos.calcar4;
+
+/**
+ * Created by Carlos on 20/04/2016.
+ */
+public class ExampleTest {
+}
