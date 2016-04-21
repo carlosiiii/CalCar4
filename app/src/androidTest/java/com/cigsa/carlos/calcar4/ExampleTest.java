7f88df29068cddcc7306840729a49e13bcd48339
carlosiiii <carlos_nompodo@hotmail.com>
2016-04-21 08:42:08 +0200

    otro commit de prueba para test

--- app/src/androidTest/java/com/cigsa/carlos/calcar4/ExampleTest.java
+++ app/src/androidTest/java/com/cigsa/carlos/calcar4/ExampleTest.java
@@ -1,7 +1,14 @@
 package com.cigsa.carlos.calcar4;
 
+import android.test.InstrumentationTestCase;
+
 /**
  * Created by Carlos on 20/04/2016.
  */
-public class ExampleTest {
+public class ExampleTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
 }
